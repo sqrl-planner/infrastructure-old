e27ea984d8685be3c0e160d21575f9456cc30fe3
Eamon Ma <m@eamonma.com>
2023-03-09 16:33:56 -0500

    Provision 1 Hetzner cloud instance in Ashburn

--- main.tf
+++ main.tf
@@ -8,11 +8,46 @@ terraform {
       name = "infrastructure"
     }
   }
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "1.36.2"
+    }
+  }
 }
 
-# An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing at all!"
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+# resource "hcloud_network" "network" {
+#   name     = "network"
+#   ip_range = "10.0.0.0/16"
+# }
+
+# resource "hcloud_network_subnet" "subnet" {
+#   network_id   = hcloud_network.network.id
+#   type         = "cloud"
+#   network_zone = "us-east"
+#   ip_range     = "10.0.1.0/24"
+# # }
+
+resource "hcloud_primary_ip" "main" {
+  name          = "main"
+  datacenter    = "ash-dc1"
+  type          = "ipv4"
+  assignee_type = "server"
+  auto_delete   = false
+}
+
+resource "hcloud_server" "web1" {
+  name        = "web1"
+  image       = "ubuntu-22.04"
+  server_type = "cpx11"
+  datacenter  = "ash-dc1"
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+    ipv4         = hcloud_primary_ip.main.id
   }
 }
